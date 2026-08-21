76981e72504c55a5fee36c253e71bc2d6452a1d8
MuggySocc <164446189+MuggySocc@users.noreply.github.com>
2026-08-20 21:44:05 -0400

    add speech processing tests

--- test_speech.py
+++ test_speech.py
@@ -1,6 +1,9 @@
 import speech
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+import numpy as np
+import config
+
 
 class TestSpeech(unittest.TestCase):
 
@@ -13,9 +16,34 @@ class TestSpeech(unittest.TestCase):
                 mock_add_dll.assert_called_once_with("C:\\fake\\cuda\\bin")
 
     def test_cpu(self):
-        with patch("speech.get_cuda", return_value = None) as mock_cpu:
+        with patch("speech.get_cuda", return_value = None) as mock_get_cuda:
             with patch("speech.os.add_dll_directory") as mock_add_dll:
                 result = speech.get_device()
                 self.assertEqual(result, "cpu")
                 mock_add_dll.assert_not_called()
 
+    def test_process_recording_valid_command(self):
+        fake_segment = MagicMock()
+        fake_segment.text = "launch steam"
+        fake_model = MagicMock()
+        fake_model.transcribe.return_value = ([fake_segment], None)
+        fake_recording = np.array([[0.0]])
+
+        with patch("speech.model", fake_model):
+            with patch("speech.launcher.launch_application") as mock_launch:
+                speech.process_recording(fake_recording)
+                mock_launch.assert_called_once_with(config.applications["steam"])
+
+    def test_process_recording_invalid_command(self):
+        fake_segment = MagicMock()
+        fake_segment.text = "launch potato"
+
+        fake_model = MagicMock()
+        fake_model.transcribe.return_value = ([fake_segment], None)
+
+        fake_recording = np.array([[0.0]])
+
+        with patch("speech.model", fake_model):
+            with patch("speech.launcher.launch_application") as mock_launch:
+                speech.process_recording(fake_recording)
+                mock_launch.assert_not_called()
